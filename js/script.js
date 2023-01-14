d7dd7053ffbee03cb5dbef50e6a803f0ef181683
natali095 <kalininanataliia095@gmail.com>
2023-01-14 18:42:12 +0000

    second commmit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,61 @@
+// Mouse Circle
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
-//Mouse Circle
+
 const mouseCircleFn = (x,y) => {
     mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity:1`;
     mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity:1`;
 };
+//End of Mouse Circle
+
+// //Animated Circles
+// const circle = document.querySelectorAll(".main-circle");
+// const mainImg = document.querySelector(".main-circle img");
+
+// let mX = 0;
+// let mY = 0;
+
+// const animateCircle = (e,x,y) => {
+
+//     // if(x < mX) {
+//     //     circle.forEach((circle) => {
+//     //         circle.style.left = `100px`;
+//     //     });
+//     //     mainImg.style.left = `100px;`;
+//     //     }
+    
+
+//     // if(y < mY) {
+//     //     console.log("move upwards");
+//     // }
+
+//     // mX = e.clientX
+//     // mXy = e.clientY
+// };
+
+// //End of Animated Circles
 
 document.body.addEventListener("mousemove", (e) => {
     let x = e.clientX;
     let y = e.clientY;
 
     mouseCircleFn(x,y);
+    // animateCircle(e,x,y);
 });
 
 document.body.addEventListener("mouseleave", () => {
     mouseCircle.style.opacity = "0";
     mouseDot.style.opacity = "0";
 });
+
+//Main Button
+const mainBtn = document.querySelector(".main-btn");
+
+mainBtn.addEventListener("mouseenter", (e) => {
+    const left = e.clientX - e.target.getBoundingClientRect
+    ().left;
+
+
+});
+//End of Main Button
