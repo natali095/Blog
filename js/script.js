e61904aaa503203b2f3533ea0d14a1f25e2938d0
natali095 <kalininanataliia095@gmail.com>
2023-01-18 18:46:59 +0000

    problem event listener

--- js/script.js
+++ js/script.js
@@ -9,53 +9,41 @@ const mouseCircleFn = (x,y) => {
 };
 //End of Mouse Circle
 
-// //Animated Circles
-// const circle = document.querySelectorAll(".main-circle");
-// const mainImg = document.querySelector(".main-circle img");
 
-// let mX = 0;
-// let mY = 0;
 
-// const animateCircle = (e,x,y) => {
-
-//     // if(x < mX) {
-//     //     circle.forEach((circle) => {
-//     //         circle.style.left = `100px`;
-//     //     });
-//     //     mainImg.style.left = `100px;`;
-//     //     }
+// document.body.addEventListener("mousemove", (e) => {
+//     let x = e.clientX;
+//     let y = e.clientY;
     
+//     mouseCircleFn(x,y);
 
-//     // if(y < mY) {
-//     //     console.log("move upwards");
-//     // }
+// });
 
-//     // mX = e.clientX
-//     // mXy = e.clientY
-// };
+// document.body.addEventListener("mouseleave", (e) => {
+//     mouseCircle.style.opacity = "0";
+//     mouseDot.style.opacity = "0";
+// });
 
-// //End of Animated Circles
-
-document.body.addEventListener("mousemove", (e) => {
-    let x = e.clientX;
-    let y = e.clientY;
-
-    mouseCircleFn(x,y);
-    // animateCircle(e,x,y);
-});
-
-document.body.addEventListener("mouseleave", () => {
-    mouseCircle.style.opacity = "0";
-    mouseDot.style.opacity = "0";
-});
-
-//Main Button
+// //Main Button
 const mainBtn = document.querySelector(".main-btn");
 
-mainBtn.addEventListener("mouseenter", (e) => {
-    const left = e.clientX - e.target.getBoundingClientRect
-    ().left;
+    let ripple;
+
+    mainBtn.addEventListener("mouseenter",(e) =>{
+    const left = e.clientX - e.target.getBoundingClientRect().left;
+    const top = e.clientY - e.target.getBoundingClientRect().top;
+    
+
+    ripple = document.createElement("div");
+    ripple.classList.add("ripple");
+    ripple.style.left = `${left}px`;
+    ripple.style.top = `${top}px`;
+    mainBtn.prepend(ripple);
+    });
+    
+   mainBtn.addEventListener("mouseleave",() => {
+       mainBtn.removeChild(ripple);
+    });
 
 
-});
 //End of Main Button
