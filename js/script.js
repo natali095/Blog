aac097e4fc2ff75207b4192be51bb1ca793561dd
natali095 <kalininanataliia095@gmail.com>
2023-02-03 21:35:00 +0000

    commit 03-02-23

--- js/script.js
+++ js/script.js
@@ -1,49 +1,36 @@
-// Mouse Circle
-const mouseCircle = document.querySelector(".mouse-circle");
-const mouseDot = document.querySelector(".mouse-dot");
-
-
-const mouseCircleFn = (x,y) => {
-    mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity:1`;
-    mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity:1`;
-};
-//End of Mouse Circle
+/*Popover button*/
+// Ця функція працює
+setTimeout((function() { 
+    $('[data-toggle="popover"]').popover();
+  }),500);
 
 
 
-// document.body.addEventListener("mousemove", (e) => {
-//     let x = e.clientX;
-//     let y = e.clientY;
-    
-//     mouseCircleFn(x,y);
+  // Ця не працює і не показує помилку
+$(document).ready(function () {
+  $('#btnSubmit').click(function () {
+    $('#myAlert').show('fade');
+  });
+  
+  $('#linkClose').click(function(){
+    $('#myAlert').hide('fade');
+  })
+});
+ 
 
-// });
+/*dome code here*/       
+  // function display(){
+  //   alert("it is nice to see you");
+  //   display1();
 
-// document.body.addEventListener("mouseleave", (e) => {
-//     mouseCircle.style.opacity = "0";
-//     mouseDot.style.opacity = "0";
-// });
+  //   var x = Hello; //dataType:number
 
-// //Main Button
-const mainBtn = document.querySelector(".main-btn");
+  //   x = true; //dataType:boolean
 
-    let ripple;
-
-    mainBtn.addEventListener("mouseenter",(e) =>{
-    const left = e.clientX - e.target.getBoundingClientRect().left;
-    const top = e.clientY - e.target.getBoundingClientRect().top;
-    
-
-    ripple = document.createElement("div");
-    ripple.classList.add("ripple");
-    ripple.style.left = `${left}px`;
-    ripple.style.top = `${top}px`;
-    mainBtn.prepend(ripple);
-    });
-    
-   mainBtn.addEventListener("mouseleave",() => {
-       mainBtn.removeChild(ripple);
-    });
-
-
-//End of Main Button
+  //   function display1(){
+  //     alert("To add your story, click on the button Add blog");
+  
+  //     var x = 10;
+  //     return x;
+  // }
+  // }
