de4fb3f5c1222c245e7dec07a2f3734f2456c12e
natali095 <kalininanataliia095@gmail.com>
2023-02-04 23:25:10 +0000

    git ignore added

--- js/script.js
+++ js/script.js
@@ -1,36 +1,26 @@
 /*Popover button*/
-// Ця функція працює
+
 setTimeout((function() { 
     $('[data-toggle="popover"]').popover();
   }),500);
 
 
-
-  // Ця не працює і не показує помилку
-$(document).ready(function () {
+/* Submit button*/
+ 
+$(document).ready (function () {
   $('#btnSubmit').click(function () {
     $('#myAlert').show('fade');
-  });
+
+
+  setTimeout(function () {
+    $('#myAlert').hide('fade');
+  }, 2000);
+});
   
   $('#linkClose').click(function(){
     $('#myAlert').hide('fade');
-  })
+  });
 });
  
 
-/*dome code here*/       
-  // function display(){
-  //   alert("it is nice to see you");
-  //   display1();
 
-  //   var x = Hello; //dataType:number
-
-  //   x = true; //dataType:boolean
-
-  //   function display1(){
-  //     alert("To add your story, click on the button Add blog");
-  
-  //     var x = 10;
-  //     return x;
-  // }
-  // }
